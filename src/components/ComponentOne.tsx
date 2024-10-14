7d64d0287ca893d547f076793eadf421340a0147
ScottGreenhalgh <scottgreenhalgh62@me.com>
2024-10-14 12:00:13 +0100

    basic project layout

---
+++ src/components/ComponentOne.tsx
@@ -0,0 +1,10 @@
+import style from "@/styles/this.module.css";
+
+const ComponentOne: React.FC = () => {
+  return (
+    <div>
+      <p className={style["this"]}>One</p>
+    </div>
+  );
+};
+export default ComponentOne;
