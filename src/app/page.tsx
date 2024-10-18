d3122b4d22a5c13bc0d9988b109815ac47939cd6
Matt Vandersluys <matt.matiksaudio@gmail.com>
2024-10-18 03:47:55 +0100

    added pokemon card demo, cleaned up css on root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ProductionImageComponent from "@/components/ProductionImageComponent";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gradient-to-tr from-indigo-500 to-rose-400 h-svh w-full">
       <ProductionImageComponent />
     </div>
   );
